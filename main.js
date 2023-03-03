0a54f7fb709040e31dbf0370ee3e97b77408b34a
Arief Setyo Nugroho <49858542+ASNProject@users.noreply.github.com>
2023-03-04 02:58:07 +0700

    update

--- main.js
+++ main.js
@@ -13,6 +13,7 @@
 // limitations under the License.
 const mobileMenu = document.querySelector(".mobile_menu");
 const navMenu = document.querySelector(".navigation");
+let navbar = document.querySelector(".navbar_list");
 
 mobileMenu.addEventListener("click", () => {
   navMenu.classList.add("show_menu");
@@ -20,4 +21,51 @@ mobileMenu.addEventListener("click", () => {
 
 navMenu.addEventListener("click", () => {
   navMenu.classList.remove("show_menu");
+});
+
+var btn_3d = document.getElementById("btn_3d");
+var btn_pcb = document.getElementById("btn_pcb");
+var btn_product = document.getElementById("btn_product");
+var btn_app = document.getElementById("btn_app");
+var btn_rnd = document.getElementById("btn_rnd");
+var prints = document.getElementById("print");
+var pcb = document.getElementById("pcb");
+var product_industrial = document.getElementById("product_industrial");
+var app = document.getElementById("application");
+var rnd = document.getElementById("rnd");
+
+btn_3d.addEventListener("click", () => {
+  prints.style.display = "flex";
+  pcb.style.display = "none";
+  product_industrial.style.display = "none";
+  app.style.display = "none";
+  rnd.style.display = "none";
+});
+btn_pcb.addEventListener("click", () => {
+  prints.style.display = "none";
+  pcb.style.display = "flex";
+  product_industrial.style.display = "none";
+  app.style.display = "none";
+  rnd.style.display = "none";
+});
+btn_product.addEventListener("click", () => {
+  prints.style.display = "none";
+  pcb.style.display = "none";
+  product_industrial.style.display = "flex";
+  app.style.display = "none";
+  rnd.style.display = "none";
+});
+btn_app.addEventListener("click", () => {
+  prints.style.display = "none";
+  pcb.style.display = "none";
+  product_industrial.style.display = "none";
+  app.style.display = "flex";
+  rnd.style.display = "none";
+});
+btn_rnd.addEventListener("click", () => {
+  prints.style.display = "none";
+  pcb.style.display = "none";
+  product_industrial.style.display = "none";
+  app.style.display = "none";
+  rnd.style.display = "flex";
 });
